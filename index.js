c6da3d7d45f19722744e958ffe76fae220cbb12a
itsNinja125 <161391272+itsNinja125@users.noreply.github.com>
2024-02-27 01:28:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ require("./bot.js")
    
    # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
    ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
+   ## Contact    [ DISCORD SERVER :  https://discord.gg/KJx79aBdJt ]
+   ## YT : https://discord.gg/KJx79aBdJt
 */
